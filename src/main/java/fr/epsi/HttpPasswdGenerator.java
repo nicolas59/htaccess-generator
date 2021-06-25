f0eca0f5b3cb38258338eed65324543da88e95cb
Nicolas <nicolas.rousseau1@gmail.com>
2021-06-25 08:57:23 +0200

    Update HttpPasswdGenerator.java

--- src/main/java/fr/epsi/HttpPasswdGenerator.java
+++ src/main/java/fr/epsi/HttpPasswdGenerator.java
@@ -7,7 +7,7 @@ public class HttpPasswdGenerator {
     public static void main(String[] args) {
         if(args.length != 2){
             System.err.println("Veuillez passer un identifiant et un mot de passe");
-            System.exit(-1);
+            System.exit(1);
             return;
         }
         String salt = BCrypt.gensalt(9);
